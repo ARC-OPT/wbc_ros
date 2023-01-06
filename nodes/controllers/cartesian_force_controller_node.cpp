81e635a3efc7c62cf66d1e830efd0145acc939b6
dmronga <dennis.mronga@dfki.de>
2023-01-06 09:37:02 +0100

    Finish force controller and add to test.launch

--- nodes/controllers/cartesian_force_controller_node.cpp
+++ nodes/controllers/cartesian_force_controller_node.cpp
@@ -18,6 +18,58 @@ CartesianForceControllerNode::CartesianForceControllerNode(int argc, char** argv
 
     controller = new CartesianForcePIDController();
 
+    if(!ros::param::has("p_gain")){
+        ROS_ERROR("WBC parameter p_gain has not been set");
+        abort();
+    }
+    std::vector<double> p_gain;
+    ros::param::get("p_gain", p_gain);
+
+    if(!ros::param::has("i_gain")){
+        ROS_ERROR("WBC parameter i_gain has not been set");
+        abort();
+    }
+    std::vector<double> i_gain;
+    ros::param::get("i_gain", i_gain);
+
+    if(!ros::param::has("d_gain")){
+        ROS_ERROR("WBC parameter d_gain has not been set");
+        abort();
+    }
+    std::vector<double> d_gain;
+    ros::param::get("d_gain", d_gain);
+
+    if(!ros::param::has("windup")){
+        ROS_ERROR("WBC parameter windup has not been set");
+        abort();
+    }
+    std::vector<double> windup;
+    ros::param::get("windup", windup);
+
+    PIDCtrlParams pid_params;
+    pid_params.p_gain = Eigen::Map<Eigen::VectorXd>(p_gain.data(),p_gain.size());
+    pid_params.i_gain = Eigen::Map<Eigen::VectorXd>(i_gain.data(),i_gain.size());
+    pid_params.d_gain = Eigen::Map<Eigen::VectorXd>(d_gain.data(),d_gain.size());
+    pid_params.windup = Eigen::Map<Eigen::VectorXd>(windup.data(),windup.size());
+
+    controller->setPID(pid_params);
+
+    if(!ros::param::has("max_control_output")){
+        ROS_ERROR("WBC parameter max_control_output has not been set");
+        abort();
+    }
+    std::vector<double> max_control_output;
+    ros::param::get("max_control_output", max_control_output);
+    controller->setMaxCtrlOutput(Eigen::Map<Eigen::VectorXd>(max_control_output.data(),max_control_output.size()));
+
+    if(!ros::param::has("dead_zone")){
+        ROS_ERROR("WBC parameter dead_zone has not been set");
+        abort();
+    }
+    std::vector<double> dead_zone;
+    ros::param::get("dead_zone", dead_zone);
+    controller->setDeadZone(Eigen::Map<Eigen::VectorXd>(dead_zone.data(),dead_zone.size()));
+
     // controller setpoint
     sub_setpoint = nh->subscribe("setpoint", 1, &CartesianForceControllerNode::setpointCallback, this);
     // controller feedback
