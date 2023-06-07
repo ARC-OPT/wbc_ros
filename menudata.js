236accaa06e5e2f0d2a5ad05a0055ea99ca0544b
dmronga <dmronga@users.noreply.github.com>
2023-06-07 06:48:58 +0000

    deploy: fff8cbd475ae4f075d4ca0228118c9c57e9b4ea2

--- menudata.js
+++ menudata.js
@@ -68,8 +68,7 @@ var menudata={children:[
 {text:"q",url:"functions_vars.html#index_q"},
 {text:"r",url:"functions_vars.html#index_r"},
 {text:"s",url:"functions_vars.html#index_s"},
-{text:"t",url:"functions_vars.html#index_t"},
-{text:"w",url:"functions_vars.html#index_w"}]}]}]},
+{text:"t",url:"functions_vars.html#index_t"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
