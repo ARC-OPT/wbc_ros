f1d23db2013b7c1fdc83c95eea0cf4170e3bfbca
dmronga <dennis.mronga@dfki.de>
2023-08-08 22:36:15 +0200

    WIP

---
+++ launch/ros2_control.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution,Command,FindExecutable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    iiwa_controllers = PathJoinSubstitution([FindPackageShare('wbc_ros'), 'config', 'iiwa_controllers.yaml'])
+    robot_description = Command([
+        PathJoinSubstitution([FindExecutable(name='xacro')]),' ',
+        PathJoinSubstitution([FindPackageShare('wbc_ros'), 'models', 'urdf', 'iiwa.config.xacro']),' ',
+        'prefix:=',                          '/',                      ' ',
+        'use_sim:=',                         'false',                  ' ',
+        'use_fake_hardware:=',               'true',                   ' ',
+        'initial_positions:=',               'initial_positions.yaml', ' ',
+        'command_interface:=',               'position',               ' ',
+        'namespace:=',                       '/'])
+    robot_description = {'robot_description': robot_description}
+
+    return LaunchDescription([
+        Node(
+            package='controller_manager',
+            executable='ros2_control_node',
+            parameters=[robot_description, iiwa_controllers],
+            output='both',
+            namespace='/'
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['whole_body_controller', '--controller-manager', ['/', 'controller_manager']],
+            namespace='/'
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_state_broadcaster', '--controller-manager', ['/', 'controller_manager']],
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace='/',
+            output='both',
+            parameters=[robot_description],
+        )
+    ])
--- src/whole_body_controller.cpp
+++ src/whole_body_controller.cpp
@@ -1,19 +1,18 @@
-#include "wbc_node.hpp"
+#include "wbc_ros/whole_body_controller.hpp"
 #include <wbc/solvers/qpoases/QPOasesSolver.hpp>
 #include <wbc/core/RobotModel.hpp>
 #include <wbc/core/QPSolver.hpp>
 #include "conversions.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+#include "pluginlib/class_list_macros.hpp"
 
 using namespace std;
 using namespace wbc;
-using namespace rclcpp;
 
 namespace wbc_ros{
 
-WbcNode::WbcNode(const rclcpp::NodeOptions &options) : ControllerNode("wbc", options), has_floating_base_state(false){
+WholeBodyController::WholeBodyController() : ControllerInterface(), has_floating_base_state(false){
 
-    declare_parameter("integrate", true);
+    /*declare_parameter("integrate", true);
     integrate = get_parameter("integrate").as_bool();
 
     RobotModelConfig robot_model_cfg;
@@ -107,26 +106,26 @@ WbcNode::WbcNode(const rclcpp::NodeOptions &options) : ControllerNode("wbc", opt
     // Input joint state
     joint_state.resize(robot_model->noOfJoints());
     joint_state.names = robot_model->jointNames();
-    sub_feedback = create_subscription<sensor_msgs::msg::JointState>("joint_states", 1, bind(&WbcNode::jointStateCallback, this, placeholders::_1));
+    sub_feedback = create_subscription<sensor_msgs::msg::JointState>("joint_states", 1, bind(&WholeBodyController::jointStateCallback, this, placeholders::_1));
 
     // Input floating base state
     if(robot_model_cfg.floating_base)
-        sub_floating_base = create_subscription<wbc_msgs::msg::RigidBodyState>("floating_base_state", 1, bind(&WbcNode::floatingBaseStateCallback, this, placeholders::_1));
+        sub_floating_base = create_subscription<wbc_msgs::msg::RigidBodyState>("floating_base_state", 1, bind(&WholeBodyController::floatingBaseStateCallback, this, placeholders::_1));
 
     // Input references, task weights and activations
     for(auto w : task_config){
         if(w.type == cart){
-            function<void(const wbc_msgs::msg::RigidBodyState& msg)> fcn = bind(&WbcNode::cartReferenceCallback, this, placeholders::_1, w.name);
+            function<void(const wbc_msgs::msg::RigidBodyState& msg)> fcn = bind(&WholeBodyController::cartReferenceCallback, this, placeholders::_1, w.name);
             sub_cart_ref.push_back(create_subscription<wbc_msgs::msg::RigidBodyState>("ref_" + w.name, 1, fcn));
         }
         else{
-            function<void(const trajectory_msgs::msg::JointTrajectory& msg)> fcn = bind(&WbcNode::jntReferenceCallback, this, placeholders::_1, w.name);
+            function<void(const trajectory_msgs::msg::JointTrajectory& msg)> fcn = bind(&WholeBodyController::jntReferenceCallback, this, placeholders::_1, w.name);
             sub_jnt_ref.push_back(create_subscription<trajectory_msgs::msg::JointTrajectory>("ref_" + w.name, 1, fcn));
         }
-        function<void(const std_msgs::msg::Float64MultiArray& msg)> fcn = bind(&WbcNode::taskWeightsCallback, this, placeholders::_1, w.name);
+        function<void(const std_msgs::msg::Float64MultiArray& msg)> fcn = bind(&WholeBodyController::taskWeightsCallback, this, placeholders::_1, w.name);
         sub_weights.push_back(create_subscription<std_msgs::msg::Float64MultiArray>("weights_" + w.name, 1, fcn));
 
-        function<void(const std_msgs::msg::Float64& msg)> fcn2 = bind(&WbcNode::taskActivationCallback, this, placeholders::_1, w.name);
+        function<void(const std_msgs::msg::Float64& msg)> fcn2 = bind(&WholeBodyController::taskActivationCallback, this, placeholders::_1, w.name);
         sub_activation.push_back(create_subscription<std_msgs::msg::Float64>("activation_" + w.name, 1, fcn2));
     }
 
@@ -143,7 +142,7 @@ WbcNode::WbcNode(const rclcpp::NodeOptions &options) : ControllerNode("wbc", opt
         publishers_task_info.push_back(create_publisher<wbc_msgs::msg::TaskStatus>("task_" + w.name, 1));
 
     // Input joint weights
-    sub_joint_weights = create_subscription<std_msgs::msg::Float64MultiArray>("joint_weights", 1, bind(&WbcNode::jointWeightsCallback, this, placeholders::_1));
+    sub_joint_weights = create_subscription<std_msgs::msg::Float64MultiArray>("joint_weights", 1, bind(&WholeBodyController::jointWeightsCallback, this, placeholders::_1));
 
     // Stats on computation time
     pub_timing_stats = create_publisher<wbc_msgs::msg::WbcTimingStats>("timing_stats", 1);
@@ -156,13 +155,65 @@ WbcNode::WbcNode(const rclcpp::NodeOptions &options) : ControllerNode("wbc", opt
     has_setpoint = true;
     stamp = get_clock()->now();
 
-    RCLCPP_INFO(get_logger(), "WBC Node is running");
+    RCLCPP_INFO(get_logger(), "WBC Node is running");*/
 }
 
-WbcNode::~WbcNode(){
+WholeBodyController::~WholeBodyController(){
 }
 
-void WbcNode::jointStateCallback(const sensor_msgs::msg::JointState& msg){
+controller_interface::InterfaceConfiguration WholeBodyController::command_interface_configuration() const{
+    controller_interface::InterfaceConfiguration conf;
+    return conf;
+}
+
+controller_interface::InterfaceConfiguration WholeBodyController::state_interface_configuration() const{
+    controller_interface::InterfaceConfiguration conf;
+    return conf;
+}
+
+controller_interface::return_type WholeBodyController::update(const rclcpp::Time & time, const rclcpp::Duration & period){
+    return controller_interface::return_type::OK;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_init(){
+    try{
+        // Create the parameter listener and read all ROS parameters
+        param_listener = std::make_shared<whole_body_controller::ParamListener>(get_node());
+        params = param_listener->get_params();
+    }
+    catch (const std::exception & e){
+        RCLCPP_ERROR(get_node()->get_logger(), "Exception thrown during init: %s \n", e.what());
+        return CallbackReturn::ERROR;
+    }
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_configure(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_activate(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_deactivate(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_cleanup(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_error(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn WholeBodyController::on_shutdown(const rclcpp_lifecycle::State & previous_state){
+    return CallbackReturn::SUCCESS;
+}
+
+
+/*void WholeBodyController::jointStateCallback(const sensor_msgs::msg::JointState& msg){
     fromROS(msg,joint_state);
     if(robot_model->hasFloatingBase()){
         if(has_floating_base_state)
@@ -172,36 +223,36 @@ void WbcNode::jointStateCallback(const sensor_msgs::msg::JointState& msg){
         has_feedback = true;
 }
 
-void WbcNode::cartReferenceCallback(const wbc_msgs::msg::RigidBodyState& msg, const string& constraint_name){
+void WholeBodyController::cartReferenceCallback(const wbc_msgs::msg::RigidBodyState& msg, const string& constraint_name){
     fromROS(msg, reference_cart);
     scene->setReference(constraint_name, reference_cart);
 }
 
-void WbcNode::jntReferenceCallback(const trajectory_msgs::msg::JointTrajectory& msg, const string& constraint_name){
+void WholeBodyController::jntReferenceCallback(const trajectory_msgs::msg::JointTrajectory& msg, const string& constraint_name){
     fromROS(msg, reference_jnt);
     scene->setReference(constraint_name, reference_jnt);
 }
 
-void WbcNode::taskActivationCallback(const std_msgs::msg::Float64& msg, const string& constraint_name){
+void WholeBodyController::taskActivationCallback(const std_msgs::msg::Float64& msg, const string& constraint_name){
     scene->setTaskActivation(constraint_name, msg.data);
 }
 
-void WbcNode::taskWeightsCallback(const std_msgs::msg::Float64MultiArray& msg, const string& constraint_name){
+void WholeBodyController::taskWeightsCallback(const std_msgs::msg::Float64MultiArray& msg, const string& constraint_name){
     fromROS(msg, task_weights);
     scene->setTaskWeights(constraint_name, task_weights);
 }
 
-void WbcNode::jointWeightsCallback(const std_msgs::msg::Float64MultiArray& msg){
+void WholeBodyController::jointWeightsCallback(const std_msgs::msg::Float64MultiArray& msg){
     fromROS(msg, robot_model->jointNames(), joint_weights);
     scene->setJointWeights(joint_weights);
 }
 
-void WbcNode::floatingBaseStateCallback(const wbc_msgs::msg::RigidBodyState& msg){
+void WholeBodyController::floatingBaseStateCallback(const wbc_msgs::msg::RigidBodyState& msg){
     fromROS(msg, floating_base_state);
     has_floating_base_state = true;
 }
 
-void WbcNode::updateController(){
+void WholeBodyController::updateController(){
     timing_stats.desired_period = 1.0 / control_rate;
     timing_stats.actual_period = (get_clock()->now() - stamp).seconds();
     stamp = get_clock()->now();
@@ -231,15 +282,14 @@ void WbcNode::updateController(){
     timing_stats.header.stamp = get_clock()->now();
     pub_timing_stats->publish(timing_stats);
 }
-
-void WbcNode::publishSolverOutput(){
+void WholeBodyController::publishSolverOutput(){
     toROS(solver_output, solver_output_ros);
     solver_output_publisher->publish(solver_output_ros);
     toROS(scene->getSolverOutputRaw(), solver_output_raw);
     solver_output_raw_publisher->publish(solver_output_raw);
 }
 
-void WbcNode::publishTaskStatus(){
+void WholeBodyController::publishTaskStatus(){
     int idx_cart = 0, idx_jnt = 0;
     for(uint i = 0; i < task_config.size(); i++){
         const TaskConfig& w = task_config[i];
@@ -254,15 +304,15 @@ void WbcNode::publishTaskStatus(){
     }
 }
 
-void WbcNode::publishTaskInfo(){
+void WholeBodyController::publishTaskInfo(){
     task_status_msgs.resize(tasks_status.size());
     for(uint i = 0; i < tasks_status.size(); i++){
         const TaskStatus& w = tasks_status[i];
         toROS(w, task_status_msgs[i]);
         publishers_task_info[i]->publish(task_status_msgs[i]);
     }
-}
+}*/
 
 }
 
-RCLCPP_COMPONENTS_REGISTER_NODE(wbc_ros::WbcNode)
+PLUGINLIB_EXPORT_CLASS(wbc_ros::WholeBodyController, controller_interface::ControllerInterface)
