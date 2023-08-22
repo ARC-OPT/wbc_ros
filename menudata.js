25106b4fd668b9ab1e1258f1004c9adeec36ccda
dmronga <dmronga@users.noreply.github.com>
2023-08-22 14:27:59 +0000

    deploy: 2ebf5f7a06c0da0aef7197aa05439d4c85f7f17b

--- menudata.js
+++ menudata.js
@@ -30,55 +30,16 @@ var menudata={children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Hierarchy",url:"hierarchy.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html",children:[
-{text:"c",url:"functions.html#index_c"},
-{text:"f",url:"functions.html#index_f"},
-{text:"h",url:"functions.html#index_h"},
-{text:"i",url:"functions.html#index_i"},
-{text:"j",url:"functions.html#index_j"},
-{text:"l",url:"functions.html#index_l"},
-{text:"n",url:"functions.html#index_n"},
-{text:"p",url:"functions.html#index_p"},
-{text:"q",url:"functions.html#index_q"},
-{text:"r",url:"functions.html#index_r"},
-{text:"s",url:"functions.html#index_s"},
-{text:"t",url:"functions.html#index_t"},
-{text:"u",url:"functions.html#index_u"},
-{text:"w",url:"functions.html#index_w"},
-{text:"~",url:"functions.html#index__7E"}]},
-{text:"Functions",url:"functions_func.html",children:[
-{text:"c",url:"functions_func.html#index_c"},
-{text:"f",url:"functions_func.html#index_f"},
-{text:"j",url:"functions_func.html#index_j"},
-{text:"l",url:"functions_func.html#index_l"},
-{text:"p",url:"functions_func.html#index_p"},
-{text:"s",url:"functions_func.html#index_s"},
-{text:"t",url:"functions_func.html#index_t"},
-{text:"u",url:"functions_func.html#index_u"},
-{text:"w",url:"functions_func.html#index_w"},
-{text:"~",url:"functions_func.html#index__7E"}]},
-{text:"Variables",url:"functions_vars.html",children:[
-{text:"c",url:"functions_vars.html#index_c"},
-{text:"f",url:"functions_vars.html#index_f"},
-{text:"h",url:"functions_vars.html#index_h"},
-{text:"i",url:"functions_vars.html#index_i"},
-{text:"j",url:"functions_vars.html#index_j"},
-{text:"n",url:"functions_vars.html#index_n"},
-{text:"p",url:"functions_vars.html#index_p"},
-{text:"q",url:"functions_vars.html#index_q"},
-{text:"r",url:"functions_vars.html#index_r"},
-{text:"s",url:"functions_vars.html#index_s"},
-{text:"t",url:"functions_vars.html#index_t"}]}]}]},
+{text:"All",url:"functions.html"},
+{text:"Functions",url:"functions_func.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
 {text:"All",url:"globals.html",children:[
 {text:"f",url:"globals.html#index_f"},
-{text:"j",url:"globals.html#index_j"},
 {text:"m",url:"globals.html#index_m"},
 {text:"t",url:"globals.html#index_t"}]},
 {text:"Functions",url:"globals_func.html",children:[
 {text:"f",url:"globals_func.html#index_f"},
 {text:"m",url:"globals_func.html#index_m"},
-{text:"t",url:"globals_func.html#index_t"}]},
-{text:"Variables",url:"globals_vars.html"}]}]}]}
+{text:"t",url:"globals_func.html#index_t"}]}]}]}]}
